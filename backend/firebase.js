f4c66ff6c5469b048e3b7af95f3e70d3636f6b7c
INES_ROCIO <quizhpiines@gmail.com>
2026-07-29 17:10:58 -0500

    Configurar Firebase usando variables de entorno

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
-const serviceAccount = require("./lumya-cd461-firebase-adminsdk-fbsvc-940730b394.json");
+
+const serviceAccount = require(process.env.FIREBASE_KEY);
 
 const app = initializeApp({
   credential: cert(serviceAccount),
